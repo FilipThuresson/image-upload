b01ced4f67b27ce937e47a76d35739d0fc230136
FilipThureson <filip.tthuresson@gmail.com>
2021-11-27 12:44:44 +0100

    Api -user

---
+++ models/User.js
@@ -0,0 +1,12 @@
+
+// models/User.js
+
+import mongoose from 'mongoose'
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password : String
+})
+
+module.exports = mongoose.models.User || mongoose.model('User', UserSchema)
